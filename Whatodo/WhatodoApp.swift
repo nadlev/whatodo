047d25f75b279390a3679c7747ca6a08217decc8
nadlev <nadiya.levytska.jr@gmail.com>
2023-04-11 14:58:24 +0300

    Initial Commit

---
+++ Whatodo/WhatodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatodoApp.swift
+//  Whatodo
+//
+//  Created by Надежда Левицкая on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
